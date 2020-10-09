ff4f55d5761cd2be4319115c229e51318aa1e090
Fabio Mendonça <fabio.mendonca.f@gmail.com>
2020-10-09 09:44:55 -0300

    Fundamentos

---
+++ fundamentos/strings.js
@@ -0,0 +1,13 @@
+const escola = "Cod3r"
+
+console.log(escola.charAt(3))
+console.log(escola.charCodeAt(3))
+console.log(escola.indexOf(3))
+console.log(escola.substring(1))
+console.log(escola.substring(0,3)) // Va do índice 0 e retorne 3 caracteres
+
+console.log('Escola '.concat(escola).concat("!"))
+console.log(escola.replace(3, 'e'))
+
+console.log('Ana,Maria,Pedro'.split(','))
+
---
+++ fundamentos/templeteString.js
@@ -0,0 +1,14 @@
+const nome = 'Rebeca'
+const concatenacao = 'olá ' + nome + '!'
+const template = `Olá ${nome} !`
+
+console.log(concatenacao)
+console.log(template)
+
+//Expressões
+
+console.log(`1 + 1 = ${1 + 1}`)
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up('Cuidado')}!`)
+
