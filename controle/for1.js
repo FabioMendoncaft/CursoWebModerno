3f9e22e31d10e4a80db5cbf44d5e937d68ff32c8
Fabio Mendonça <fabio.mendonca.f@gmail.com>
2020-10-26 11:08:59 -0300

    estruturas de repetição

---
+++ controle/for1.js
@@ -0,0 +1,23 @@
+let cont = 1
+
+while(cont <= 10){
+    console.log(`Contador = ${cont}`)
+    cont++
+}
+console.log(' ')
+for(let i = 1; i <= 10; i++){
+    console.log(`Contador = ${i}`)
+}
+console.log(' ')
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+
+for(let i = 0; i < notas.length; i++){
+    console.log(`Notas = ${notas[i]}`)
+}
+
+console.log(' ')
+var n = 5
+for(let i = 1; i <= 10; i++){
+    console.log(`${n} * ${i} = ${n * i}`)
+}
+
